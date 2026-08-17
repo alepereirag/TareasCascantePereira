c62a222282dc1ab236a52c8f37f58cd02410d912
alepereirag <ale.pereira.g@icloud.com>
2026-08-17 17:10:53 -0600

    Corrige los 3 errores de flake8 del archivo utilidades_extra

--- Tarea 1/utilidades_extra.py
+++ Tarea 1/utilidades_extra.py
@@ -1,17 +1,30 @@
-"""Modulo extra de utilidades (VERSION CON 3 ERRORES DE FLAKE8).
+"""Modulo extra de utilidades (VERSION CORREGIDA).
 
-Este archivo se agrega en un branch a proposito con 3 errores que
-flake8 puede detectar, para luego corregirlos en un commit posterior.
+Esta es la version del archivo con los 3 errores de flake8 ya
+corregidos:
+  1. Se elimino el import no utilizado (F401).
+  2. Se agregaron espacios alrededor del operador "=" (E225).
+  3. Se acorto la linea que superaba los 79 caracteres (E501).
 """
-
-import math  # ERROR 1 (F401): import no utilizado en ningun lugar.
 
 
 def sumar(a, b):
-    resultado=a + b  # ERROR 2 (E225): falta espacio alrededor del "=".
+    """Suma dos numeros y devuelve el resultado.
+
+    :param a: primer sumando.
+    :param b: segundo sumando.
+    :return: la suma de a y b.
+    """
+    resultado = a + b  # Corregido: espacios alrededor del "=".
     return resultado
 
 
 def saludar(nombre):
-    mensaje = "Hola " + nombre + ", este es un mensaje demasiado largo que supera los limites"  # ERROR 3 (E501): linea muy larga.
+    """Construye un mensaje de saludo para el nombre dado.
+
+    :param nombre: nombre de la persona a saludar.
+    :return: mensaje de saludo.
+    """
+    # Corregido: la linea se divide para no superar los 79 caracteres.
+    mensaje = "Hola " + nombre + ", este es un mensaje de saludo"
     return mensaje
